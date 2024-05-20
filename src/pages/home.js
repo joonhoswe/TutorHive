e55ffa7a89083cfd6530d193a5d1dbc9c3cb0b13
joonhoswe <joonhoswe@gmail.com>
2024-05-20 00:51:32 -0400

    added basic accordion FAQ page

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ export default function Home() {
 
 
   return (
-    <div className="text-white flex flex-col justify-center items-center overflow-hidden relative w-full min-h-screen" 
+    <div className="text-white bg-yellow-150 flex flex-col justify-center items-center overflow-hidden relative w-full min-h-screen" 
       style={{ 
       // backgroundImage: `url(${background})`,
       // backgroundSize: 'cover', 
